b9eda8dee86e441219ca6bb495699ea3ed0108db
Matt Williams <matt@milliams.com>
2017-03-15 19:09:17 +0000

    Simplify examples to not create unnecessary Vec

--- examples/histogram_svg.rs
+++ examples/histogram_svg.rs
@@ -1,7 +1,7 @@
 extern crate plotlib;
 
 fn main() {
-    let data = vec![0.3, 0.5, 6.4, 5.3, 3.6, 3.6, 3.5, 7.5, 4.0];
+    let data = [0.3, 0.5, 6.4, 5.3, 3.6, 3.6, 3.5, 7.5, 4.0];
     let h = plotlib::histogram::Histogram::from_vec(&data, 10);
     let v = plotlib::view::View::new().add(&h);
     plotlib::plot::Plot::single(&v).save("histogram.svg");
--- examples/scatter_text.rs
+++ examples/scatter_text.rs
@@ -1,7 +1,7 @@
 extern crate plotlib;
 
 fn main() {
-    let data = vec![(-3.0, 2.3), (-1.6, 5.3), (0.3, 0.7), (4.3, -1.4), (6.4, 4.3), (8.5, 3.7)];
+    let data = [(-3.0, 2.3), (-1.6, 5.3), (0.3, 0.7), (4.3, -1.4), (6.4, 4.3), (8.5, 3.7)];
     let s = plotlib::scatter::Scatter::from_vec(&data);
     let v = plotlib::view::View::new()
         .add(&s)
