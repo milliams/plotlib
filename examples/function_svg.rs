d229844c14ad9afdc5476799351344529481c326
Matt Williams <matt@milliams.com>
2018-02-26 22:30:33 +0000

    Slightly more interesting functions

--- examples/function_svg.rs
+++ examples/function_svg.rs
@@ -1,8 +1,12 @@
 extern crate plotlib;
 
 fn main() {
-    let f = plotlib::function::Function::new(|x| x, 0., 10.)
+    let f1 = plotlib::function::Function::new(|x| x * 5., 0., 10.)
         .style(plotlib::function::Style::new().colour("burlywood"));
-    let v = plotlib::view::View::new().add(&f);
+    let f2 = plotlib::function::Function::new(|x| x.powi(2), 0., 10.)
+        .style(plotlib::function::Style::new().colour("darkolivegreen"));
+    let f3 = plotlib::function::Function::new(|x| x.sqrt() * 20., 0., 10.)
+        .style(plotlib::function::Style::new().colour("brown"));
+    let v = plotlib::view::View::new().add(&f1).add(&f2).add(&f3);
     plotlib::page::Page::single(&v).save("function.svg");
 }
