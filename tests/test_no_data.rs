5f79861cb24154aca4f1e9bb3791920808b91dd4
Simon Walker <s.r.walker101@googlemail.com>
2018-07-20 08:21:07 +0100

    Add test for plot with no data

--- tests/test_no_data.rs
+++ tests/test_no_data.rs
@@ -30,3 +30,28 @@ fn test_data_with_one_length() {
     Page::single(&v)
         .save("/tmp/scatter_one_length.svg");
 }
+
+#[test]
+fn test_data_with_no_length() {
+    // Scatter plots expect a list of pairs
+    let data1 = [];
+
+    // We create our scatter plot from the data
+    let s1 = Scatter::from_slice(&data1).style(
+        scatter::Style::new()
+            .marker(Marker::Square) // setting the marker to be a square
+            .colour("#DD3355"),
+    ); // and a custom colour
+
+    // The 'view' describes what set of data is drawn
+    let v = ContinuousView::new()
+        .add(&s1)
+        .x_range(-5., 10.)
+        .y_range(-2., 6.)
+        .x_label("Some varying variable")
+        .y_label("The response of something");
+
+    // A page with a single view is then saved to an SVG file
+    Page::single(&v)
+        .save("/tmp/scatter_zero_length.svg");
+}
