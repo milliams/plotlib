2794ef851c1779d3f4fcaf4b27788d51205b72c0
Simon Walker <s.r.walker101@googlemail.com>
2019-04-01 19:50:23 +0100

    Include LineJoin configuration in example

--- examples/line_svg.rs
+++ examples/line_svg.rs
@@ -1,11 +1,14 @@
-use plotlib::repr::Line;
 use plotlib::page::Page;
-use plotlib::style::LineStyle;
+use plotlib::repr::Line;
+use plotlib::style::{LineJoin, LineStyle};
 use plotlib::view::ContinuousView;
 
 fn main() {
-    let l1 = Line::new(&[(0., 1.), (2., 1.5), (3., 1.2), (4., 1.1)])
-        .style(LineStyle::new().colour("burlywood"));
+    let l1 = Line::new(&[(0., 1.), (2., 1.5), (3., 1.2), (4., 1.1)]).style(
+        LineStyle::new()
+            .colour("burlywood")
+            .linejoin(LineJoin::Round),
+    );
     let v = ContinuousView::new().add(&l1);
     Page::single(&v).save("line.svg").expect("saving svg");
 }
